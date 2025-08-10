0fec3681a78f3e3c4b5a8089221a042f124d4a49
Anurag Raj <adityanv8@gmail.com>
2025-08-10 16:01:36 +0530

    complete Create page

--- frontend/src/pages/Createpage.tsx
+++ frontend/src/pages/Createpage.tsx
@@ -1,7 +1,9 @@
 import { useState, type FormEvent } from "react"
 import ToogleButton from "../Components/ToogleButton"
-import Closepage from "../ui/Closepage"
 import { useNavigate } from "react-router-dom"
+import Button from "../ui/Button"
+import { IoArrowUndoSharp } from "react-icons/io5"
+import '../Components/mainpage.css'
 
 const Createpage = () => {
 
@@ -54,45 +56,54 @@ const Createpage = () => {
     setTags(remove)
   }
 
+
+  function Navigation() {
+    navigate('/main')
+  }
+
   return (
     <div>
-      <ToogleButton></ToogleButton>
-      <Closepage></Closepage>
-      <div className=" lg:flex lg:flex-col lg:w-full lg:h-screen lg:bg-amber-300  lg:justify-center lg:items-center">
-        <div className=" lg:text-7xl">Create Page</div>
-        <form onSubmit={handleSubmit}>
-          <div>
-          <label htmlFor="title">Title</label>
-          <input required type="text" id="title" placeholder="Enter Title" value={title} onChange={(e)=>setTitle(e.target.value)}></input>
+      <div onClick={Navigation}>
+      <Button type="primary" size="md" startIcon={<IoArrowUndoSharp size={30} />}>Back to Main Page</Button>
+      </div>
+      <div className=" lg:flex  lg:h-screen lg:flex-col lg:w-full  lg:bg-black lg:text-white lg:transition-all lg:duration-1000  lg:justify-center lg:items-center"> 
+        <form onSubmit={handleSubmit} className="glass lg:px-28 lg:py-12 lg:flex lg:flex-col lg:gap-y-10 ">
+          <div className=" lg:flex flex-col">
+          <label htmlFor="title" className=" lg:text-3xl ">Title</label>
+          <input required type="text" className=" lg:border-b-2 lg:w-[40vw] lg:text-2xl  "  id="title" placeholder="Enter Title" value={title} onChange={(e)=>setTitle(e.target.value)}></input>
           </div>
-          <div>
-          <label htmlFor="link">Link</label>
-          <input required type="text" id="link" placeholder="Enter link" value={link} onChange={(e)=>setLink(e.target.value)}></input>
+          <div  className=" lg:flex flex-col">
+          <label  htmlFor="link"  className=" lg:text-3xl ">Link</label>
+          <input  className=" lg:border-b-2 lg:w-[40vw] lg:text-2xl  "  required type="text" id="link" placeholder="Enter link" value={link} onChange={(e)=>setLink(e.target.value)}></input>
           </div>
-          <div>
-          <label htmlFor="link">Type</label>
-          <select id="link"  value={type} onChange={(e)=>setType(e.target.value)}>
+          <div className=" lg:flex lg:flex-col lg:gap-y-3">
+          <label className="lg:text-3xl  lg:flex flex-col" htmlFor="link">Type</label>
+          <select className=" lg:bg-black lg:text-white" id="link"  value={type} onChange={(e)=>setType(e.target.value)}>
             <option value="pdf">Pdf</option>
             <option value="audio">Audio</option>
             <option value="video">Video</option>
             <option value="article">Article</option>
             <option value="image">Image</option>
           </select>
-          <label htmlFor="tag">Tags</label>
+          <div className=" lg:flex flex-col">
+          <label  className=" lg:text-3xl "   htmlFor="tag">Tags</label>
+          <div className=" lg:flex lg:gap-x-[40vh]">
           <input type="text" placeholder="Enter tag" value={tag} onChange={(e)=>setTag(e.target.value)}></input>
-          <button type="button" onClick={HandleAddtag}>Add Tag</button>
+          <button  className=" lg:bg-white lg:text-black lg:border lg:rounded-full lg:px-3 lg:py-1 lg:hover:bg-black lg:hover:text-white lg:transition-all lg:duration-500 " type="button" onClick={HandleAddtag}>Add Tag</button>
           </div>
-          <div>
+          </div>
+          </div>
+          <div className=" lg:flex lg:gap-x-1 lg:flex-wrap  lg:w-[40vw]">
             {
               tags.map((tag:string)=>(
-                <div key={tag}>
-                  <div>{tag}</div>
-                  <button  type="button"  onClick={()=>HandleRemovetag(tag)}>X</button>
+                <div key={tag} className=" lg:flex lg:justify-start lg:items-center lg:gap-x-1">
+                  <div className=" lg:text-xl">{tag}</div>
+                  <button className=" lg:bg-white lg:text-black lg:border lg:rounded-full lg:px-3 lg:py-1 lg:hover:bg-black lg:hover:text-white lg:transition-all lg:duration-500 "  type="button"  onClick={()=>HandleRemovetag(tag)}>X</button>
                 </div>
               ))
             }
           </div>
-          <button type="submit" >Submit</button>
+          <button className=" lg:bg-white lg:text-black  lg:rounded-full lg:px-3 lg:py-1 lg:hover:bg-black lg:hover:text-white lg:transition-all lg:duration-500 "  type="submit" >Submit</button>
         </form>
       </div>
     </div>
