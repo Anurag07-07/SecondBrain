12d46a236af90a568f16d3704a18e96755125b0a
Anurag Raj <adityanv8@gmail.com>
2025-08-11 19:53:10 +0530

    complete everything only left with unique feature

--- frontend/src/Components/Mainpage.tsx
+++ frontend/src/Components/Mainpage.tsx
@@ -1,22 +1,23 @@
 import LogoutButton from './LogoutButton'
 import ToogleButton from './ToogleButton'
 import './mainpage.css'
-import { useEffect, useRef, useState } from 'react'
-import axios, { AxiosError } from 'axios'
+import { useEffect, useState } from 'react'
+import axios from 'axios'
 import Card from '../pages/Card'
 import Navbar from '../pages/Navbar'
 
 type userIdProps = {
-  id:string,
+  _id:string | undefined,
   username:string
 }
 export interface IData{
+  _id:string
   title:string,
   link:string,
   type:string,
   tags?:string[],
   description?:string,
-  userId:userIdProps
+  userId?:userIdProps
 }
 
 const Mainpage = () => {
@@ -24,64 +25,44 @@ const Mainpage = () => {
   const [data,setData] = useState<IData[]>([])
 
   
-  const timeRef = useRef<ReturnType<typeof setTimeout> | null>(null)
+  // const timeRef = useRef<ReturnType<typeof setTimeout> | null>(null)
 
-  useEffect(()=>{
-
-    //Function to get data from the backend
-    async function GetData() {
-      try {
-      
-        const response =  await axios.get('http://localhost:3000/api/v1/view_content',{
-        headers:{
-          'authorization':`Bearer ${localStorage.getItem('token')}`
-        }
-      })
+  async function GetData() {
+    try {
+      const response = await axios.get("http://localhost:3000/api/v1/view_content", {
+        headers: {
+          authorization: `Bearer ${localStorage.getItem("token")}`,
+        },
+      });
 
       if (response.data) {
-        //Here response.data is returning data but we have to extract array which is data
-        setData(response.data.data)
+        console.log(response.data);
+        setData([...response.data.data]);
       }
-        
-      } catch (error) {
-        const err = error as AxiosError<{type:string}>
-        if (err.response) {
-          console.log(err.response.data);
-        }else{
-          console.log(err.message);
-          
-        }
-      }
-    }
-
-
-    GetData()
-
-    //Call database after eaach 10 sec so that if someone added something we get data in next refresh
-   timeRef.current = setInterval(GetData,10*1000)
-
-   return ()=>{
-    //Unmounting
-    if (timeRef.current) {
-      clearInterval(timeRef.current)
+    } catch (error) {
+      console.error("Error fetching data:", error);
     }
    }
-  },[])
+
+   useEffect(() => {
+    GetData();
+   }, [data]);
 
   return (
-    <div className='  lg:w-full lg:max-h-min lg:dark:bg-black lg:dark:text-white lg:transition-all lg:duration-500'>
+    <>
       <ToogleButton></ToogleButton>
       <LogoutButton></LogoutButton>
-        {/* Navbar */}
-        <Navbar></Navbar>
-        <div className=' lg:flex lg:flex-wrap lg:gap-y-6 lg:py-4  lg:p-24 lg:w-full lg:h-max '>
+      <Navbar></Navbar>
+    <div className='  lg:w-full lg:min-h-screen lg:dark:bg-black lg:dark:text-white lg:transition-all lg:duration-500'>
+        <div className=' lg:flex  lg:flex-wrap   lg:w-full lg:justify-around lg:gap-y-6   '>
           {
             data.map((c)=>(
-              <Card description={c.description} key={c.userId.id} tags={c.tags} title={c.title} link={c.link} type={c.type} userId={c.userId}   ></Card>
+              <Card getCard={GetData} description={c.description}  tags={c.tags} title={c.title} key={c._id}  link={c.link} type={c.type} _id={c._id}   ></Card>
             ))
           }
         </div>
     </div>
+    </>
   )
 }
 
--- frontend/src/pages/Card.tsx
+++ frontend/src/pages/Card.tsx
@@ -1,32 +1,63 @@
 import { useGSAP } from "@gsap/react"
 import type { IData } from "../Components/Mainpage"
-
+import { IoIosTrash } from "react-icons/io";
 import './card.css'
 import gsap from "gsap"
+import axios, {  AxiosError } from "axios";
+import { useRef } from "react";
 interface ICard extends IData{
   bgcolor?:"black" |"red",
-  textcolor?:"white"
+  textcolor?:"white",
+  getCard?:()=>void
 }
 
 
-const Card = ({title,link,tags,type,userId,description}:ICard) => {
+const Card = ({title,link,tags,type,_id,description,getCard}:ICard) => {
 
-  useGSAP(()=>{
-    gsap.from('#card',{
-      opacity:0,
-      y:30
+  const cardRef = useRef<HTMLDivElement>(null)
+
+  useGSAP(() => {
+    gsap.fromTo(cardRef.current, {
+      opacity: 0,
+      y: 30
+    },
+    {
+      y: -10,
+      opacity: 1,
+      duration: 1.2,
+    });
+  },[]);
+
+
+  async function handleDeleteCard(e:string) {
+    try {
+      const response = await axios.delete(`http://localhost:3000/api/v1/delete_content/${e}`,{
+        headers:{
+          'authorization':`Bearer ${localStorage.getItem('token')}`
+        }
+      })
+
+  
+      if (response.data) {
+        console.log(response.data);
+        if (getCard) {
+          getCard()
+        }
+      }
+      
+    } catch (error) {
+      const err = error as AxiosError<{type:string}>
+      if (err.message) {
+        console.log(err.response?.status);
+      }else{
+        console.error(err.response?.data);
+        
+      }
     }
-    )
-    gsap.to('#card',{
-      y:-10,
-      opacity:1,
-      duration:1.2,
-      stagger:0.2
-    })
-  })
+  }
 
   return (
-    <div id="card" className="  lg:m-4 lg:flex lg:flex-col lg:flex-wrap lg:gap-y-3  lg:w-[25vw] lg:justify-center lg:items-start lg:rounded-4xl   lg:p-10 lg:bg-gradient-to-bl from-gray-300 via-darkgrey-100  ">
+    <div ref={cardRef} className=" card-container lg:relative   lg:flex lg:flex-col lg:flex-wrap lg:gap-y-3  lg:w-[25vw] lg:justify-center lg:items-start lg:rounded-4xl   lg:p-10 lg:bg-gradient-to-bl from-gray-300 via-darkgrey-100  ">
       <div><span>Title: </span>{title}</div>
       <div className=" lg:font-extralight">{description ?<div><span>Description: </span>{description}</div>:<span></span>}</div>
       <a href={link}><span>Link:</span>{link}</a>
@@ -36,6 +67,7 @@ const Card = ({title,link,tags,type,userId,description}:ICard) => {
           <span>{`#${d}   `}</span>
         ))
       }</div>
+      <button onClick={()=>handleDeleteCard(_id)}  className=" lg:absolute lg:top-5 lg:right-8 lg:hover:bg-red-600 lg:transition-all lg:duration-500 lg:rounded-2xl lg:p-2">{<IoIosTrash size={20}></IoIosTrash>}</button>
     </div>
   )
 }
